f7759a13e380dfbf4595acf096c2de07872bc617
tompujalte <tom.pujalte@gmail.com>
2023-12-06 08:38:34 -0500

    build test 1

--- src/index.jsx
+++ src/index.jsx
@@ -14,7 +14,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
         <Banner />
         <Routes>
           <Route path="/" element={<Home />} />
